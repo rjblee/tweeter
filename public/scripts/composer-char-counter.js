625666e59f7317eac3ff8d7d56742ad1714b1f90
Ralph <ralph.lee@live.ca>
2019-07-17 02:11:16 +0000

    Added character length counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  console.log("whatever");
+  $('.new-tweet textarea').on("keyup", function(event) {
+      const charCounter = $(this).parent().children('div').children('span');
+      const remainingChar = 140-$(this).val().length;
+
+      if (remainingChar < 0) {
+        charCounter.addClass("redCounter");
+      } else if (charCounter.hasClass("redCounter") && remainingChar >= 0) {
+        charCounter.removeClass("redCounter");
+      }
+      
+      // Sets the counter to the remaining value.
+      $(charCounter).text(remainingChar);
+
+  })
 });
+
