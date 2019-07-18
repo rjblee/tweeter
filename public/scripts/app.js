1239e6b06d37fbb6c88521ae2513f0271cc6e4c8
Ralph <ralph.lee@live.ca>
2019-07-18 01:05:42 +0000

    Completed dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,81 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(() => {
+  const createTweetElement = function(obj) {
+    const $article = $("<article>").addClass("tweetPost");
+    
+    const $header = $("<header>").addClass("tweetHeader");
+    $article.append($header);
+    
+    const $div1 = $("<div>").addClass("avatarAndName");
+    $header.append($div1);
+  
+    const $img1 = $("<img>").attr(obj.user.avatars);
+    $div1.append($img1);
 
+    const $span1 = $("<span>").text(obj.user.name);
+    $div1.append($span1);
+
+    const $span2 = $("<span>").addClass("username").text(obj.user.handle)
+    $header.append($span2);
+
+    const $p1 = $("<p>").addClass("tweetText").text(obj.content.text);
+    $article.append($p1);
+
+    const $footer = $("<footer>").addClass("tweetFooter");
+    $article.append($footer);
+    
+    const $hr = $("<hr>");
+    $footer.append($hr);
+
+    const $p2 = $("<p>").text(obj.created_at);
+    $footer.append($p2);
+
+    const $div2 = $("<div>").addClass("icons");
+    $footer.append($div2);
+
+    const $img2 = $("<img>");
+    $div2.append($img2);
+
+    const $img3 = $("<img>");
+    $div2.append($img3);
+
+    const $img4 = $("<img>");
+    $div2.append($img4);
+
+  
+    return $article;
+  }
+  
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" 
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $article = createTweetElement(tweet);
+      $("#tweets-container").append($article);
+    }
+  };
+
+  renderTweets(tweetData);
+});
